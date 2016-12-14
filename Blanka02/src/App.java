d00afcd186343cea052dde51c75d8f819c5d5fcd
blankapreradovic <blankapreradovic@gmail.com>
2016-12-14 11:16:07 +0100

    Add files via upload

---
+++ Blanka02/src/App.java
@@ -0,0 +1,27 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int myNumber = 2;
+		short myShort = 452;
+		long myLong = 799;
+		double myDouble = 8.763;
+		float myFloat = 942.3f;
+		char myChar = 'y';
+		boolean myBoolean = true;
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+	
+	}
+
+}	
